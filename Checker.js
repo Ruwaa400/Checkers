97dab05f1c1568c0b409c580d6b1fa30d402a885
Hiba-alkurd <hibaalkurd@gmail.com>
2020-11-14 22:24:26 +0200

    checker

--- Checker.js
+++ Checker.js
@@ -1,9 +1,10 @@
 class Checker {
-    constructor(board, ele, location) {
+    constructor(board, ele, location, ID) {
         this.board = board;
         this.element = ele;
         this.location = location;   // array[row, col]
         this.king = false;
+        this.ID = ID;
         this.movable = false;
         // who has the checker
         if (currBoard.board[this.location[0]][this.location[1]] == 1) {
@@ -28,7 +29,61 @@ class Checker {
             }
 
         });
+     };
 
-    };
+    movable(player){
+        let opp = (player == 1? 2 : 1);
+        let row = this.location[0];
+        let col = this.location[1];
+        //can only go down
+        if(player == 1 || this.king){
+            //down left
+            if( row < 7 && col > 0 && (this.board[row+1][col-1] == 0) ){
+                return true;
+            }
+            //down right
+            if( row < 7 && col < 7 && (this.board[row+1][col+1] == 0) ){
+                return true;
+            }
+            //if there's jump
+            //down left
+            if( row < 6 && col > 1 && (this.board[row+1][col-1] == opp) ){
+                if(this.board[row+2][col-2] == 0){
+                    return true;
+                }
+            }
+            //down right
+            if( row < 6 && col < 6 && (this.board[row+1][col+1] == opp) ){
+                if(this.board[row+2][col+2] == 0){
+                    return true;
+                }
+            }
+        }
+        else if( player == 2 || this.king){
+            //top left
+            if( row > 0 && col > 0 && (this.board[row-1][col-1] == 0) ){
+                return true;
+            }
+            //top right
+            if( row > 0 && col > 0 && (this.board[row-1][col+1] == 0) ){
+                return true;
+            }
+            //if there's jump
+            //top left
+            if( row > 1 && col > 1 && (this.board[row-1][col-1] == opp) ){
+                if(this.board[row-2][col-2] == 0){
+                    return true;
+                }
+            }
+            //top right
+            if( row > 1 && col > 1 && (this.board[row-1][col+1] == opp) ){
+                if(this.board[row-2][col+2] == 0){
+                    return true;
+                }
+            }
+       
+        }
+        return false;
+    }
 
 };
